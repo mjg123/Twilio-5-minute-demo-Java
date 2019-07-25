83017b8def4e351f36b4ceff182e710334f38f16
Matthew Gilliard <matthew@gilliard.lol>
2019-07-25 13:05:56 +0100

    initial state for SMS raffle version

---
+++ src/main/java/lol/gilliard/FiveMinuteDemo.java
@@ -0,0 +1,16 @@
+package lol.gilliard;
+
+import static spark.Spark.get;
+
+public class FiveMinuteDemo {
+
+    // Contest Phone number is: 
+
+    private static String message = "Thanks for entering the IntelliJ license raffle";
+
+    public static void main(String[] args) {
+
+//        get("/sms", (req, res) -> "<Response><Message>" + message + "</Message></Response>");
+
+    }
+}
