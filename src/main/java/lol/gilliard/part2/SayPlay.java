4e64c8844366ab4196c60ba603c51c6983962a3b
Matthew Gilliard <matthew@gilliard.lol>
2020-12-01 11:14:48 +0000

    finished SayPlay pt2

--- src/main/java/lol/gilliard/part2/SayPlay.java
+++ src/main/java/lol/gilliard/part2/SayPlay.java
@@ -2,8 +2,15 @@ package lol.gilliard.part2;
 
 import com.twilio.Twilio;
 import com.twilio.base.ResourceSet;
+import com.twilio.rest.api.v2010.account.Call;
 import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Play;
+import com.twilio.twiml.voice.Say;
 import com.twilio.type.PhoneNumber;
+import com.twilio.type.Twiml;
+
+import java.util.HashSet;
 
 import static lol.gilliard.part1.RespondToSMS.PHONE_NUMBER;
 
@@ -15,13 +22,25 @@ public class SayPlay {
             System.getenv("ACCOUNT_SID"),
             System.getenv("AUTH_TOKEN"));
 
-        PhoneNumber twilioNumber = new PhoneNumber(PHONE_NUMBER);
+        var myTwilioPhoneNumber = new PhoneNumber(PHONE_NUMBER);
 
-        ResourceSet<Message> allMessages = Message.reader().setTo(twilioNumber).read();
+        // fetch all messages
+        var messages = Message.reader().setTo(myTwilioPhoneNumber).read();
 
+        // get unique senders' phone numbers
+        HashSet<PhoneNumber> phoneNumbers = new HashSet<>();
+        messages.forEach(msg -> phoneNumbers.add(msg.getFrom()));
+
+        // call 'em all
+        phoneNumbers.forEach(pn ->
+            Call.creator(pn, myTwilioPhoneNumber,
+                new Twiml(
+                    new VoiceResponse.Builder()
+                        .say(new Say.Builder("Thanks for calling, have a great day").build())
+                        .play(new Play.Builder("https://demo.twilio.com/docs/classic.mp3").build())
+                        .build().toXml()
+                )).create()
+        );
     }
 
 }
-
-
-// live template: callcreator
